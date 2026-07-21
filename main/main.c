4379d6d2d6b8a5c2445247b47176cffebb6c01fe
ivanjermakov <ivanjermakov1@gmail.com>
2026-07-22 00:17:10 +0200

    Read raw temperature over i2c

--- main/main.c
+++ main/main.c
@@ -1,4 +1,5 @@
 #include "driver/gpio.h"
+#include "driver/i2c_master.h"
 #include "esp_chip_info.h"
 #include "esp_event.h"
 #include "esp_flash.h"
@@ -45,19 +46,76 @@ void wifi_connect(void) {
     printf("%s: connected\n", CONFIG_WIFI_SSID);
 }
 
-void app_main(void) {
-    nvs_flash_init();
-    esp_netif_init();
-
-    wifi_connect();
-
+void blink_led() {
     gpio_reset_pin(LED_GPIO);
     gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
     while (true) {
-        printf("LED %s\n", blink_state ? "on" : "off");
-        gpio_set_level(LED_GPIO, blink_state);
+        // printf("LED %s\n", blink_state ? "on" : "off");
+        // gpio_set_level(LED_GPIO, blink_state);
 
         vTaskDelay((blink_state ? 20 : 1000) / portTICK_PERIOD_MS);
         blink_state = !blink_state;
+    }
+}
+
+void app_main() {
+    nvs_flash_init();
+    esp_netif_init();
+
+    // wifi_connect();
+
+    i2c_master_bus_handle_t bus_handle;
+    i2c_master_dev_handle_t dev_handle;
+    i2c_master_bus_config_t bus_config = {
+        .i2c_port = I2C_NUM_0,
+        .sda_io_num = 21,
+        .scl_io_num = 22,
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+        .glitch_ignore_cnt = 7,
+        .flags.enable_internal_pullup = true,
+    };
+    i2c_new_master_bus(&bus_config, &bus_handle);
+
+    i2c_device_config_t dev_config = {
+        .dev_addr_length = I2C_ADDR_BIT_LEN_7,
+        .device_address = 0b01110110,
+        .scl_speed_hz = 100000,
+    };
+    i2c_master_bus_add_device(bus_handle, &dev_config, &dev_handle);
+
+    uint8_t write_buf[2];
+    uint8_t read_buf[8];
+
+    // soft reset
+    write_buf[0] = 0xe0;
+    write_buf[1] = 0xb6;
+    i2c_master_transmit(dev_handle, write_buf, 2, -1);
+
+    uint8_t chip_id;
+    write_buf[0] = 0xd0;
+    ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, write_buf, 1, &chip_id, 1, -1));
+    printf("chip id: 0x%02X (should be 0x60)\n", chip_id);
+
+    // config
+    write_buf[0] = 0xf5;
+    write_buf[1] = 0x00;
+    i2c_master_transmit(dev_handle, write_buf, 2, -1);
+
+    while (true) {
+        // ctrl_meas
+        write_buf[0] = 0xf4;
+        write_buf[1] = (0x01 << 5) | (0x01 << 2) | 0x01;
+        i2c_master_transmit(dev_handle, write_buf, 2, -1);
+
+        vTaskDelay(20 / portTICK_PERIOD_MS);
+
+        // read
+        write_buf[0] = 0xf7;
+        ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, write_buf, 1, read_buf, 8, -1));
+        printf("raw buf: %x %x %x %x %x %x %x %x\n", read_buf[0], read_buf[1], read_buf[2],
+               read_buf[3], read_buf[4], read_buf[5], read_buf[6], read_buf[7]);
+        int32_t raw_temp = (read_buf[3] << 12) | (read_buf[4] << 4) | (read_buf[5] >> 4);
+        printf("raw temp: %" PRId32 "\n", raw_temp);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
     }
 }
