42f3c3f3d1fd0315bc731577ab5ebedb87d567f5
ivanjermakov <ivanjermakov1@gmail.com>
2026-07-22 00:29:51 +0200

    Compensated temperature

--- main/main.c
+++ main/main.c
@@ -14,48 +14,21 @@
 
 #define LED_GPIO 2
 
-bool blink_state = false;
-static SemaphoreHandle_t semaphore_ip = NULL;
+typedef struct {
+    uint16_t t1;
+    int16_t t2;
+    int16_t t3;
+} Calibration;
 
-void got_ip_event_handler(void* arg, esp_event_base_t base, int32_t id, void* data) {
-    xSemaphoreGive(semaphore_ip);
-}
+Calibration calibration;
 
-void wifi_connect(void) {
-    printf("%s: connecting\n", CONFIG_WIFI_SSID);
-
-    esp_event_loop_create_default();
-    esp_netif_create_default_wifi_sta();
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    esp_wifi_init(&cfg);
-
-    esp_wifi_set_mode(WIFI_MODE_STA);
-
-    wifi_config_t config = {.sta = {.ssid = CONFIG_WIFI_SSID, .password = CONFIG_WIFI_PASSWORD}};
-    esp_wifi_set_config(WIFI_IF_STA, &config);
-
-    semaphore_ip = xSemaphoreCreateBinary();
-    esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &got_ip_event_handler, NULL,
-                                        NULL);
-    esp_wifi_start();
-    esp_wifi_connect();
-
-    xSemaphoreTake(semaphore_ip, portMAX_DELAY);
-    vSemaphoreDelete(semaphore_ip);
-    printf("%s: connected\n", CONFIG_WIFI_SSID);
-}
-
-void blink_led() {
-    gpio_reset_pin(LED_GPIO);
-    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
-    while (true) {
-        // printf("LED %s\n", blink_state ? "on" : "off");
-        // gpio_set_level(LED_GPIO, blink_state);
-
-        vTaskDelay((blink_state ? 20 : 1000) / portTICK_PERIOD_MS);
-        blink_state = !blink_state;
-    }
+float compensate_temperature(int32_t raw_temp) {
+    int32_t var1 = ((((raw_temp >> 3) - (calibration.t1 << 1))) * (calibration.t2)) >> 11;
+    int32_t var2 =
+        (((((raw_temp >> 4) - calibration.t1) * ((raw_temp >> 4) - calibration.t1)) >> 12) *
+         (calibration.t3)) >>
+        14;
+    return (float)(((var1 + var2) * 5 + 128) >> 8) / 100.0;
 }
 
 void app_main() {
@@ -101,6 +74,15 @@ void app_main() {
     write_buf[1] = 0x00;
     i2c_master_transmit(dev_handle, write_buf, 2, -1);
 
+    // calib
+    uint8_t calib_data_raw[6];
+    write_buf[0] = 0x88;
+    i2c_master_transmit_receive(dev_handle, write_buf, 1, calib_data_raw, 6, -1);
+    calibration.t1 = (calib_data_raw[1] << 8) | calib_data_raw[0];
+    calibration.t2 = (calib_data_raw[3] << 8) | calib_data_raw[2];
+    calibration.t3 = (calib_data_raw[5] << 8) | calib_data_raw[4];
+    printf("calibration: T1=%d, T2=%d, T3=%d\n", calibration.t1, calibration.t2, calibration.t3);
+
     while (true) {
         // ctrl_meas
         write_buf[0] = 0xf4;
@@ -112,10 +94,8 @@ void app_main() {
         // read
         write_buf[0] = 0xf7;
         ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, write_buf, 1, read_buf, 8, -1));
-        printf("raw buf: %x %x %x %x %x %x %x %x\n", read_buf[0], read_buf[1], read_buf[2],
-               read_buf[3], read_buf[4], read_buf[5], read_buf[6], read_buf[7]);
         int32_t raw_temp = (read_buf[3] << 12) | (read_buf[4] << 4) | (read_buf[5] >> 4);
-        printf("raw temp: %" PRId32 "\n", raw_temp);
+        printf("temp: %f \n", compensate_temperature(raw_temp));
         vTaskDelay(500 / portTICK_PERIOD_MS);
     }
 }
