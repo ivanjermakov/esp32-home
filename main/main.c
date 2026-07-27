a48c55fdc7d456e158cbc8d687d0bd2c10505831
ivanjermakov <ivanjermakov1@gmail.com>
2026-07-28 01:58:40 +0200

    Send NEC

--- main/main.c
+++ main/main.c
@@ -12,7 +12,8 @@
 #include "sdkconfig.h"
 #include "stdio.h"
 
-#define IR_GPIO 15
+#define IR_TX_GPIO 4
+#define IR_RX_GPIO 15
 
 QueueHandle_t ir_queue;
 uint32_t last_t = 0;
@@ -20,12 +21,12 @@ uint32_t code = 0;
 uint8_t bits = 0;
 bool active = false;
 
-typedef enum {
-    on = 0x5926cfb0,
-    off = 0x5926bdc2,
+typedef enum : uint32_t {
+    code_on = 0x5926cfb0,
+    code_off = 0x5926bdc2,
 } Code;
 
-static void IRAM_ATTR ir_intr_handler(void* arg) {
+static void IRAM_ATTR ir_rx_isr(void* arg) {
     uint32_t now = esp_timer_get_time();
     if (last_t == 0) {
         last_t = now;
@@ -57,23 +58,61 @@ static void IRAM_ATTR ir_intr_handler(void* arg) {
     }
 }
 
+static void IRAM_ATTR ir_tx_mark(uint32_t us) {
+    uint32_t n = us / 13;
+    for (uint32_t i = 0; i < n; i++) {
+        gpio_set_level(IR_TX_GPIO, 1);
+        esp_rom_delay_us(13);
+        gpio_set_level(IR_TX_GPIO, 0);
+        esp_rom_delay_us(13);
+    }
+}
+
+static void IRAM_ATTR ir_tx_space(uint32_t us) {
+    gpio_set_level(IR_TX_GPIO, 0);
+    esp_rom_delay_us(us);
+}
+
+void ir_tx_nec(uint32_t code) {
+    gpio_config_t cfg = {
+        .pin_bit_mask = 1ULL << IR_TX_GPIO,
+        .mode = GPIO_MODE_OUTPUT,
+    };
+    gpio_config(&cfg);
+
+    ir_tx_mark(9000);
+    ir_tx_space(4500);
+
+    for (int i = 31; i >= 0; i--) {
+        ir_tx_mark(560);
+        if ((code >> i) & 1) {
+            ir_tx_space(1690);
+        } else {
+            ir_tx_space(560);
+        }
+    }
+}
+
 void app_main() {
 
     ir_queue = xQueueCreate(256, sizeof(uint32_t));
     gpio_config_t cfg = {
-        .pin_bit_mask = 1ULL << IR_GPIO,
+        .pin_bit_mask = 1ULL << IR_RX_GPIO,
         .mode = GPIO_MODE_INPUT,
         .pull_up_en = GPIO_PULLUP_ENABLE,
         .intr_type = GPIO_INTR_NEGEDGE,
     };
     gpio_config(&cfg);
     gpio_install_isr_service(ESP_INTR_FLAG_IRAM);
-    gpio_isr_handler_add(IR_GPIO, ir_intr_handler, NULL);
+    gpio_isr_handler_add(IR_RX_GPIO, ir_rx_isr, NULL);
 
     while (true) {
-        uint32_t code = 0;
         if (xQueueReceive(ir_queue, &code, pdMS_TO_TICKS(100))) {
             printf("0x%08" PRIx32 "\n", code);
         }
+
+        printf("sending code %" PRIx32 "\n", code_on);
+        ir_tx_nec(code_on);
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
