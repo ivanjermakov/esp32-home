c89cd348cd03b06e0d9585bb9d77b81eda135f34
ivanjermakov <ivanjermakov1@gmail.com>
2026-07-28 13:29:55 +0200

    Refactoring

--- main/main.c
+++ main/main.c
@@ -16,50 +16,44 @@
 #define IR_RX_GPIO 15
 
 QueueHandle_t ir_queue;
-uint32_t last_t = 0;
-uint32_t code = 0;
+uint32_t bit_start_t = 0;
+uint64_t code = 0;
 uint8_t bits = 0;
-bool active = false;
 
-typedef enum : uint32_t {
-    code_on = 0x5926cfb0,
-    code_off = 0x5926bdc2,
+typedef enum : uint64_t {
+    code_on = 0x0000b24d9f6020df,
+    code_off = 0x0000b24d7b84e01f,
 } Code;
 
-static void IRAM_ATTR ir_rx_isr(void* arg) {
+void IRAM_ATTR ir_rx_isr(void* arg) {
     uint32_t now = esp_timer_get_time();
-    if (last_t == 0) {
-        last_t = now;
+    if (bit_start_t == 0) {
+        bit_start_t = now;
         return;
     }
 
-    uint32_t dt = now - last_t;
-    last_t = now;
-
+    uint32_t dt = now - bit_start_t;
     if (dt < 500) return;
+    xQueueSendFromISR(ir_queue, &dt, NULL);
 
-    if (dt > 3000 && dt < 6000) {
+    if (dt > 3000) {
         code = 0;
         bits = 0;
-        active = true;
-    } else if (active && bits < 32) {
-        if (dt < 1500)
-            code <<= 1;
-        else if (dt < 3000)
-            code = (code << 1) | 1;
-        bits++;
-        if (bits == 32) {
-            active = false;
-            xQueueSendFromISR(ir_queue, &code, NULL);
-        }
-    } else {
-        active = false;
-        bits = 0;
+        bit_start_t = now;
+        return;
+    }
+
+    code <<= 1;
+    code |= dt < 1500 ? 0 : 1;
+    bit_start_t = now;
+    bits++;
+    if (bits == 32 || bits == 48) {
+        xQueueSendFromISR(ir_queue, &code, NULL);
     }
 }
 
-static void IRAM_ATTR ir_tx_mark(uint32_t us) {
-    uint32_t n = us / 13;
+void IRAM_ATTR ir_tx_mark(uint32_t us) {
+    uint32_t n = us / 21;
     for (uint32_t i = 0; i < n; i++) {
         gpio_set_level(IR_TX_GPIO, 1);
         esp_rom_delay_us(13);
@@ -68,24 +62,19 @@ static void IRAM_ATTR ir_tx_mark(uint32_t us) {
     }
 }
 
-static void IRAM_ATTR ir_tx_space(uint32_t us) {
+void IRAM_ATTR ir_tx_space(uint32_t us) {
     gpio_set_level(IR_TX_GPIO, 0);
     esp_rom_delay_us(us);
 }
 
-void ir_tx_nec(uint32_t code) {
-    gpio_config_t cfg = {
-        .pin_bit_mask = 1ULL << IR_TX_GPIO,
-        .mode = GPIO_MODE_OUTPUT,
-    };
-    gpio_config(&cfg);
-
+void IRAM_ATTR ir_tx_nec(uint64_t code, uint8_t size) {
     ir_tx_mark(9000);
     ir_tx_space(4500);
 
-    for (int i = 31; i >= 0; i--) {
+    uint8_t offset = 64 - size;
+    for (int i = size - 1; i >= 0; i--) {
         ir_tx_mark(560);
-        if ((code >> i) & 1) {
+        if ((code >> (i + offset)) & 1) {
             ir_tx_space(1690);
         } else {
             ir_tx_space(560);
@@ -95,24 +84,32 @@ void ir_tx_nec(uint32_t code) {
 
 void ir_tx_task(void* arg) {
     while (true) {
-        printf(" > 0x%08" PRIx32 "\n", code_on);
-        ir_tx_nec(code_on);
+        printf(" > 0x%016llx\n", code_on);
+        ir_tx_nec(code_on, 48);
+        ir_tx_nec(code_on, 48);
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
 void ir_rx_task(void* arg) {
     while (true) {
-        if (xQueueReceive(ir_queue, &code, portMAX_DELAY)) {
-            printf("<  0x%08" PRIx32 "\n", code);
+        uint64_t v;
+        if (xQueueReceive(ir_queue, &v, portMAX_DELAY)) {
+            printf("<  0x%016llx (%llu)\n", v, v);
         }
     }
 }
 
 void app_main() {
+    ir_queue = xQueueCreate(64, sizeof(uint64_t));
 
-    ir_queue = xQueueCreate(256, sizeof(uint32_t));
     gpio_config_t cfg = {
+        .pin_bit_mask = 1ULL << IR_TX_GPIO,
+        .mode = GPIO_MODE_OUTPUT,
+    };
+    gpio_config(&cfg);
+
+    cfg = (gpio_config_t){
         .pin_bit_mask = 1ULL << IR_RX_GPIO,
         .mode = GPIO_MODE_INPUT,
         .pull_up_en = GPIO_PULLUP_ENABLE,
@@ -122,6 +119,6 @@ void app_main() {
     gpio_install_isr_service(ESP_INTR_FLAG_IRAM);
     gpio_isr_handler_add(IR_RX_GPIO, ir_rx_isr, NULL);
 
-    xTaskCreate(ir_tx_task, "ir_tx", 2048, NULL, 5, NULL);
-    xTaskCreate(ir_rx_task, "ir_rx", 2048, NULL, 4, NULL);
+    xTaskCreate(ir_tx_task, "ir_tx", 2048, NULL, 10, NULL);
+    xTaskCreate(ir_rx_task, "ir_rx", 2048, NULL, 5, NULL);
 }
