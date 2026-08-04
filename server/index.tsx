2d31b47b6587f60d40d529eab85b71aa3b932573
ivanjermakov <ivanjermakov1@gmail.com>
2026-08-04 23:17:34 +0200

    Web UI

---
+++ server/index.tsx
@@ -0,0 +1,11 @@
+/* @refresh reload */
+
+import { Component } from 'solid-js'
+import { render } from 'solid-js/web'
+import './index.css'
+
+const Main: Component = () => {
+    return <span>home</span>
+}
+
+render(() => <Main />, document.getElementById('root')!)
